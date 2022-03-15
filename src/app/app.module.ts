75d00d84dc4730e763c9144b19daa84b17ff7cee
nelrafa103 <60740045+nelrafa103@users.noreply.github.com>
2022-03-15 17:16:06 +0100

    Adding Navbar,Intro,advert,footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
+import { AdvertComponent } from './advert/advert.component';
+import { FooterComponent } from './footer/footer.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    AdvertComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
